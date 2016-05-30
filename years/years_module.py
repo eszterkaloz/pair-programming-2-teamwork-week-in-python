642baed39c947945f9295ae021adfa50f6db289d
eszterkaloz <eszterkaloz@gmail.com>
2016-05-30 13:35:29 +0200

    implemented basic years function

--- years/years_module.py
+++ years/years_module.py
@@ -1,12 +1,19 @@
-import datetime
+from datetime import date
 
 
 def years(age):
-    return
+    remaining_years = 100 - age
+    future_date = date.today().year + remaining_years
+    return future_date
 
 
 def main():
-    return
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+
+    hundred_years = years(age)
+    print("Hi {0}, you will be 100 years old in {1}".format(name, hundred_years))
+    # return
 
 
 if __name__ == '__main__':
