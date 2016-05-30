ca229d1ce5a892845f2d27876a43d4d4bda85cb6
eszterkaloz <eszterkaloz@gmail.com>
2016-05-30 15:43:07 +0200

    implement palindrome tester

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,22 @@
-def palindrome(str):
-    return
+def palindrome(sentence):
+    word_list = sentence.split(" ")
+    one_piece = ""
+    for i in range(len(word_list)):
+        one_piece += word_list[i]
+    lower_one_piece = one_piece.lower()
+    same = 1
+    for i in range(len(lower_one_piece)):
+        same *= (lower_one_piece[0+i] == lower_one_piece[-1-i])
+    return same
 
 
 def main():
+    sent = input("Enter a sentence to test if it is a palindrome: ")
+    result = palindrome(sent)
+    if result:
+        print('Your sentence is a palindrome.')
+    else:
+        print("Your sentence is not palindrome.")
     return
 
 
