cc5e3133cf2c06f598cdac93297a2329af2c2247
eszterkaloz <eszterkaloz@gmail.com>
2016-06-01 09:59:42 +0200

    made passwordgen pass the test

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -4,9 +4,23 @@ import string
 
 
 def passwordgen():
-    size = random.randint(8, 15)
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for x in range(size))
+    # small_letter_size = random.randint(2, 5)
+    # big_letter_size = random.randint(2, 5)
+    # digit_letter_size = random.randint(2, 5)
+    # char_letter_size = random.randint(2, 5)
+    # size = small_letter + big_letter + char_letter + digit_letter
+    #
+    # digit_letter = random.choice(string.digits)
+    # char_letter = random.choice(string.punctuation)
+
+    # characters = random.choice(string.ascii_letters) + random.choice(string.digits) + random.choice(string.punctuation)
+    # return ''.join(random.choice(characters) for x in range(size))
+
+    size = random.randint(2, 5)
+    pwd = ""
+    for i in range(size):
+        pwd = pwd + random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + random.choice("[!@#$%^&*()?]")
+    return pwd
 
 
 def main():
