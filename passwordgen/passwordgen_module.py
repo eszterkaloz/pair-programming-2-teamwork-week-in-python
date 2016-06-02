48e512f1cfa566e8193fb8fc97cdf1266bcf983f
Kristof Galla <gallakristof@gmail.com>
2016-06-02 09:39:27 +0200

    generating weak or strong passwords

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -3,7 +3,7 @@ import random
 import string
 
 
-def passwordgen():
+def passwordgen(weakness):
     # small_letter_size = random.randint(2, 5)
     # big_letter_size = random.randint(2, 5)
     # digit_letter_size = random.randint(2, 5)
@@ -15,16 +15,24 @@ def passwordgen():
 
     # characters = random.choice(string.ascii_letters) + random.choice(string.digits) + random.choice(string.punctuation)
     # return ''.join(random.choice(characters) for x in range(size))
-
-    size = random.randint(2, 5)
     pwd = ""
-    for i in range(size):
-        pwd = pwd + random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + random.choice("[!@#$%^&*()?]")
+
+    if weakness == 2:
+        size = random.randint(2, 5)
+        for i in range(size):
+            pwd = pwd + random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + random.choice("[!@#$%^&*()?]")
+
+    elif weakness == 1:
+        wordfile = open('/usr/share/dict/american-english', 'r')
+        wordlist = wordfile.readlines()
+        pwd = pwd + random.choice(wordlist)
+        wordfile.close()
     return pwd
 
 
 def main():
-    pwd = passwordgen()
+    strength = int(input("How strong would you like your password to be? (type 1 for weak, 2 for strong): "))
+    pwd = passwordgen(strength)
     print(pwd)
     return
 
